6e4775b303359006b85ac9ce899e0dbe755a3972
Jussi Alanen <juzapala@gmail.com>
2023-08-29 02:04:02 +0300

    added supports

--- web/app/mu-plugins/jussinet-plugin/src/Cpt/Project.php
+++ web/app/mu-plugins/jussinet-plugin/src/Cpt/Project.php
@@ -19,7 +19,7 @@ class Project
                 'has_archive' => true,
                 'rewrite' => ['slug' => 'projects'],
                 'show_in_rest' => true,
-                // 'supports' => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments' ),
+                'supports' => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments' ),
             )
         );
     }
